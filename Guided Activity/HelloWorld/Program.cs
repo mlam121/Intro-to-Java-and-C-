7de994c2d2db9377fdf66dfdb71670389519a2f2
mylena <mylenalam101@gmail.com>
2026-08-27 12:44:32 -0500

    Finish Guided Activity

--- Guided Activity/HelloWorld/Program.cs
+++ Guided Activity/HelloWorld/Program.cs
@@ -1,3 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, C#!");
 Console.WriteLine("My name is Mylena.");
 Console.WriteLine("I am studying Database Development.");
--- Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa2a34c8d8896577ddd814959fbcbd5316acd535")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e80d58d7bdbc72300b9d5dc82ea728dfc794ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

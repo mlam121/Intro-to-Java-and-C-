0372fcd24d8504d4588039695c3c0374f2d97948
mylena <mylenalam101@gmail.com>
2026-08-27 12:19:01 -0500

    Java Guided Activity

---
+++ Guided Activity/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("My name is Mylena!");
+        System.out.println("I am studying Database Development.");
+    }
+}

1e684954d2c195cfc6213f02bcfc24d9e36cd768
Mylena Lam <mylenalam101@gmail.com>
2026-08-31 18:40:29 -0500

    delete my challenge 1 folder

--- Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4681155304cf7a8fb2f2f2601e6c184290bd6f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b634861ec7751417112ba9fc9caae2ff3256dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

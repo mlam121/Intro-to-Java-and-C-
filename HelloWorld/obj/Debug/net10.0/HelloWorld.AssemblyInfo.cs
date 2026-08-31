c5e0c9b84862ec8e780bf4e01c8f0153867a5a5d
Mylena Lam <mylenalam101@gmail.com>
2026-08-31 18:57:47 -0500

    saving

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e684954d2c195cfc6213f02bcfc24d9e36cd768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe018054afb350f6da41aab1954883b25678c4f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

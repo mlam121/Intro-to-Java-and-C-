3616c62dd27b24eae4eff58d48bd4b7c42d39e90
Mylena Lam <mylenalam101@gmail.com>
2026-09-05 12:32:20 -0500

    finish numeric values

--- NumericTypes.java
+++ NumericTypes.java
@@ -22,11 +22,19 @@ public class NumericTypes
       String output;                // Line of output
 
       // TASK #2 declare variables used here
+      String firstName;
+      String lastName;
+      String fullName;
       // TASK #3 declare variables used here
+      char firstInitial;
+      char lastInitial;
       // TASK #4 declare variables used here
+      double diameter;
+      double radius;
+      double volume;
 
       // Find an arithmetic average.
-      average = (SCORE1 + SCORE2) / NUMBER;
+      average = (double)(SCORE1 + SCORE2) / NUMBER;
       output = SCORE1 + " and " + SCORE2 +
                " have an average of " + average;
       System.out.println(output);
@@ -42,36 +50,46 @@ public class NumericTypes
       // Prompt the user for first name
         System.out.print("Enter your first name: ");
       // Read the user's first name
-        String firstName = keyboard.nextLine();
+        firstName = keyboard.nextLine();
       // Prompt the user for last name
         System.out.print("Enter your last name: ");
       // Read the user's last name
-        String lastName = keyboard.nextLine();
+        lastName = keyboard.nextLine();
 
               System.out.println();      // To leave a blank line
 
       // Concatenate the user's first and last names
-        String fullName = firstName + " " + lastName;
+fullName = firstName + " " + lastName;
       // Print out the user's full name
-        System.out.println("Your full name is " + fullName);
-
+      System.out.println(fullName);
 
 
       // ADD LINES FOR TASK #3 HERE
       // Get the first character from the user's first name
-      char firstInitial = firstName.charAt(0);
+      firstInitial = firstName.charAt(0);
+      lastInitial = lastName.charAt(0);
       // Print out the user's first initial
+      System.out.print(firstInitial);
+      System.out.println(lastInitial);
       // Convert the user's full name to uppercase
-      
+      fullName = fullName.toUpperCase();
       // Print out the user's full name in uppercase
-
+      System.out.println(fullName);
       System.out.println();      // To leave a blank line
 
       // ADD LINES FOR TASK #4 HERE
       // Prompt the user for a diameter of a sphere
+      System.out.print("Enter the diameter: ");
       // Read the diameter
+      diameter = keyboard.nextDouble();
       // Calculate the radius
+      radius = diameter / 2;
       // Calculate the volume
+      volume = (4/3.0)*Math.PI*Math.pow(radius, 3);
       // Print out the volume
+      System.out.println("The volume of the sphere is: " + volume);
+
+      //Close the scanner object
+      keyboard.close();
    }
 }

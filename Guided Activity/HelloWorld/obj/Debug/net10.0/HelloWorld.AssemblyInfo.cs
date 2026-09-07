39dacce59db2e807ed4c383a07b93c8096ee55e9
Mylena Lam <mylenalam101@gmail.com>
2026-09-07 18:35:39 -0500

    desktop

--- Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Guided Activity/HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8a7e8d254671ecfd0928d6348bf496e0efca06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45a3a11936db145afa2a28a292336dcb1e27ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StudentStats/obj/Debug/net10.0/StudentStats.AssemblyInfo.cs
+++ StudentStats/obj/Debug/net10.0/StudentStats.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentStats")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f578b376436265f043ca8dcbffb8862c2cee0ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45a3a11936db145afa2a28a292336dcb1e27ff6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentStats")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentStats")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

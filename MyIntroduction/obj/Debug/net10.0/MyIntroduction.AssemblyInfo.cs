135e7b9d28c4205f57205763596b9ccb5798f8a8
Mylena Lam <mylenalam101@gmail.com>
2026-09-07 17:33:45 -0500

    desktop

--- MyIntroduction/obj/Debug/net10.0/MyIntroduction.AssemblyInfo.cs
+++ MyIntroduction/obj/Debug/net10.0/MyIntroduction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyIntroduction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f61a4991eb914903f1bd1296536fc780390374c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a624ad304a14656b3ccf3ea68238eb0dab80e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyIntroduction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyIntroduction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

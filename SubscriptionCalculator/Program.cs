8bb51e45f9448c490942cab4d1c952a754d59f1e
Mylena Lam <mylenalam101@gmail.com>
2026-09-14 17:09:15 -0500

    start chall 3

---
+++ SubscriptionCalculator/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Welcome to Streamfix! Subscription Calculator");
+
+//User input
+Console.Write("Enter your name: ");
+String name = Console.ReadLine();
+
+Console.Write("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+try-catch block to handle invalid input for age
+try
+{
+    Console.Write("Enter your age: ");
+    age = int.Parse(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid input. Please enter a valid integer for age.");
+    return;
+}
+
+
+
+Console.Write("Enter your subscription plan (B = Basic, S = Standard, P = Premium): ");
+String plan = Console.ReadLine();
+
+Console.Write("Enter student status (Y = yes, N = no): ");
+String studentStatus = Console.ReadLine();
+

43db743796be44854be51ecf7b2e26d0f26df542
Mylena Lam <mylenalam101@gmail.com>
2026-09-14 17:19:53 -0500

    start again

---
+++ SubscriptionCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Welcome to Streamfix! Subscription Calculator");
+
+//User input
+Console.WriteLine("Enter your name: ");
+String name = Console.ReadLine();
+
+Console.WriteLine("Enter your age: ");
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your subscription plan (B = Basic, S = Standard, P = Premium): ");
+String plan = Console.ReadLine();
+
+Console.WriteLine("Enter student status (Y = yes, N = no): ");
+String studentStatus = Console.ReadLine();

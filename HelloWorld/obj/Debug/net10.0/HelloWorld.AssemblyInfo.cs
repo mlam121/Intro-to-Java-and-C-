6bda42a13fb5b88009a3b2725aa42c1b7f0b87fa
Mylena Lam <mylenalam101@gmail.com>
2026-09-14 20:06:57 -0500

    desktop

--- HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5ea8a21d84cec76f7e1df4476178071127c2d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a418f1c4a04daeb3972b0cc0c3d117b2e8ea811")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
